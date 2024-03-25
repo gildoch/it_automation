727adc2b4bec7bd00aa1a1e5b9995096e787c957
Gildo Chauze <gildochauze@gmail.com>
2024-03-25 12:16:46 +0200

    Folder clean up

--- alchemy/connection.py
+++
@@ -1,9 +0,0 @@
-from sqlalchemy import Column, Integer, String, create_engine
-from sqlalchemy.ext.declarative import declarative_base
-
-Base = declarative_base()
-
-class Filmes(Base):
-    __tablename__ = 'filmes'
-
-    titulo = Column
