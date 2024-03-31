99aeca150e370583adbfe48918afc223da91f577
Gildo Chauze <gildochauze@gmail.com>
2024-03-31 11:49:31 +0200

    Add a simple network connectivity check

--- all_checks.py
+++ all_checks.py
@@ -3,7 +3,7 @@
 import os
 import sys
 import shutil
-
+import socket
 
 def check_reboot():
     return os.path.exists('/run/reboot-required')
@@ -23,8 +23,17 @@ def check_root_full():
     """Resturns True if disk is full"""
     return check_disk_full(disk='/', min_gb=2, min_percent=10)
 
+
+def check_no_network():
+    """Returns True if it fails to resolve Google's URL, False otherwise"""
+    try:
+        socket.gethostbyname('www.google.com')
+        return False
+    except:
+        return True
+    
 def main(): 
-    checks = [(check_reboot,"Pending Reboot."),(check_root_full,"Root partition full.")]
+    checks = [(check_reboot,"Pending Reboot."),(check_root_full,"Root partition full."),(check_no_network,"No working network")]
     everything_ok = True
 
     for check, msg in checks:
